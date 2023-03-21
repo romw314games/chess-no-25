49cac667004c467a1b1e7e973c0311efb4efb215
romw314 <106016361+romw314@users.noreply.github.com>
2023-03-21 19:25:13 +0100

    created a simple whereCanMove function that returns an empty array

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,7 +1,45 @@
+const index2pos = (index) => { return { x: index % 8, y: Math.floor(index / 8) }; };
+const pos2index = (x, y) => (y === undefined) ? pos2index(x.x, x.y) : (x + y * 8);
+
+function getSquare(squares, x, y) {
+	return squares[pos2index(x, y)];
+}
+
+function whereCanMove(squares, piece, x, y) {
+	if (y === undefined) return whereCanMove(squares, x.x, x.y);
+
+	const result = [];
+
+	const square = (x, y) => getSquare(squares, x, y);
+	const add = (x, y) => result.push(pos2index(x, y));
+	const emptyForPlayer = (x, y) => !square(x, y) || (square(x, y).player !== piece.player);
+	const addIfEmptyExt = (x, y, ext, isEmpty) => {
+		isEmpty ??= emptyForPlayer;
+		if (ext && isEmpty(x, y)) add(x, y);
+	};
+	const addIfEmpty = (x, y, isEmpty) => addIfEmptyExt(x, y, true, isEmpty);
+	const addAllIfEmpty = (all) => {
+		for (let [x, y] of all)
+			addIfEmptyExt(x, y, isValidPosition(x, y));
+	}
+	const addAllIfEmptyRelative = (all) => addAllIfEmpty(all.map(pos => [pos[0] + x, pos[1] + y]));
+	const isValidPosition = (x, y) => {
+		const range = [...Array(8).keys()];
+		return range.includes(x) && range.includes(y);
+	};
+	
+	switch (piece.name) {
+	}
+
+	console.dlog(3, `whereCanMove: test end`, result);
+
+	return result;
+}
+
 function canBeMoved(selectedPiece, endIndex, squares) {
 	if (endIndex === selectedPiece)
 		return false;
 	return selectedPiece !== null;
 }
 
-export { canBeMoved };
+export { canBeMoved, whereCanMove, index2pos, pos2index };
